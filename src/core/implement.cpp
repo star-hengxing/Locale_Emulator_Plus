0f2bf27298ee693105b858e114a7fa785fd63423
star9029 <hengxings783@gmail.com>
2023-12-21 00:19:09 +0800

    refractor: improve convert function pointer

--- src/core/implement.cpp
+++ src/core/implement.cpp
@@ -3,9 +3,10 @@
 NAMESPACE_BEGIN(hook)
 
 template <typename T>
-auto get_source_function_ptr(hook_function_help func) noexcept
+auto get_source_function_ptr(hook_function_help index) noexcept
 {
-    return (T)(HOOK_LIST[(int)func].src);
+    // TODO: c++23 std::to_underlying
+    return reinterpret_cast<T>(HOOK_LIST[static_cast<usize>(index)].src);
 }
 
 int WINAPI MessageBoxW(
@@ -14,8 +15,7 @@ int WINAPI MessageBoxW(
     _In_opt_ LPCWSTR lpCaption,
     _In_ UINT uType)
 {
-    auto const type = ::MessageBoxW;
-    auto const func = get_source_function_ptr<decltype(type)>(hook_function_help::MessageBoxW);
+    auto const func = get_source_function_ptr<decltype(&::MessageBoxW)>(hook_function_help::MessageBoxW);
     return func(hWnd, L"hook", lpCaption, uType);
 }
 
@@ -23,8 +23,7 @@ BOOL WINAPI SetWindowTextW(
     _In_ HWND hWnd,
     _In_opt_ LPCWSTR lpString)
 {
-    auto const type = ::SetWindowTextW;
-    auto const func = get_source_function_ptr<decltype(type)>(hook_function_help::SetWindowTextW);
+    auto const func = get_source_function_ptr<decltype(&::SetWindowTextW)>(hook_function_help::SetWindowTextW);
     return func(hWnd, L"hook");
 }
 
